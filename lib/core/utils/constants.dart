8f339a8c6ee9549d79addb4b53ffe27be4b8bcd2
FajarSubeki <fajarsub06@gmail.com>
2024-02-29 19:15:14 +0700

    create constant for setup server and app message

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  const Constants._();
+
+  static const String kBaserUrl = 'https://api.rawg.io/api/';
+  static const String apiKey = '9b8ab7bf2f064ff59c6cf6668e6ce8fc';
+
+  static const String serveFailureMessage = 'Server Failure';
+  static const String unexpectedFailureMessage = 'Unexpected Failure';
+  static const String noInternetMessage = 'No Internet Connection';
+
+}
